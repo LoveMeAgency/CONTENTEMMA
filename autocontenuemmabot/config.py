a7c9f1eeaf7b3f10f413a9aa98316364ffc9b4ab
LoveMeAgency <admin@lovemeagency.com>
2025-12-05 18:32:11 +0100

    Add files via upload

---
+++ autocontenuemmabot/config.py
@@ -0,0 +1,77 @@
+# ----- Identifiants Telegram -----
+API_ID = "14703744"
+API_HASH = "f49c3172164036fbf04432e096d444e8"
+
+# Admin & Bot token
+ADMIN_ID = 6529167025
+BOT_TOKEN_1 = "8012198352:AAEadUjEX2rDOJaLyRz4jPYL5g75oh1Kz0I"
+
+# ----- Canaux ciblés -----
+# Mets ici les IDs de tes canaux (en int). Sur Telegram, les channels commencent souvent par -100...
+CHANNEL_IDS = [
+    -1003151893255,
+    # -1009876543210,
+]
+
+# ----- Fuseau & suppression -----
+TIMEZONE = "Europe/Malta"   # ex: "Europe/Paris"
+AUTO_DELETE_AFTER_DAYS = 7  # suppression au bout d'1 semaine
+
+# ----- Planning par post (jour + HH:MM) -----
+POST1_SCHEDULE = ("lundi", "18:47")
+POST2_SCHEDULE = ("lundi", "18:50")
+POST3_SCHEDULE = ("lundi", "18:55")
+POST4_SCHEDULE = ("lundi", "18:56")
+POST5_SCHEDULE = ("lundi", "19:03")
+POST6_SCHEDULE = ("lundi", "19:07")
+POST7_SCHEDULE = ("lundi", "19:12")
+POST8_SCHEDULE = ("lundi", "19:15")
+POST9_SCHEDULE = ("mardi", "13:08")
+POST10_SCHEDULE = ("mardi", "13:14")
+POST11_SCHEDULE = ("mardi", "13:16")
+POST12_SCHEDULE = ("mardi", "13:20")
+POST13_SCHEDULE = ("mardi", "13:45")
+POST14_SCHEDULE = ("mardi", "13:48")
+POST15_SCHEDULE = ("mardi", "13:53")
+POST16_SCHEDULE = ("mardi", "13:57")
+POST17_SCHEDULE = ("mardi", "21:11")
+POST18_SCHEDULE = ("mardi", "21:20")
+POST19_SCHEDULE = ("mardi", "21:33")
+POST20_SCHEDULE = ("mardi", "22:16")
+POST21_SCHEDULE = ("mercredi", "17:20")
+POST22_SCHEDULE = ("mercredi", "17:22")
+POST23_SCHEDULE = ("mercredi", "17:23")
+POST24_SCHEDULE = ("mercredi", "17:24")
+POST25_SCHEDULE = ("mercredi", "17:26")
+POST26_SCHEDULE = ("mercredi", "17:27")
+POST27_SCHEDULE = ("mercredi", "17:31")
+POST28_SCHEDULE = ("mercredi", "17:38")
+POST29_SCHEDULE = ("jeudi", "19:52")
+POST30_SCHEDULE = ("jeudi", "19:58")
+POST31_SCHEDULE = ("jeudi", "20:02")
+POST32_SCHEDULE = ("jeudi", "20:04")
+POST33_SCHEDULE = ("jeudi", "20:05")
+POST34_SCHEDULE = ("jeudi", "20:08")
+POST35_SCHEDULE = ("jeudi", "20:09")
+POST36_SCHEDULE = ("jeudi", "20:15")
+POST37_SCHEDULE = ("vendredi", "22:02")
+POST38_SCHEDULE = ("vendredi", "22:04")
+POST39_SCHEDULE = ("vendredi", "22:06")
+POST40_SCHEDULE = ("vendredi", "22:10")
+POST41_SCHEDULE = ("vendredi", "22:13")
+POST42_SCHEDULE = ("vendredi", "22:27")
+POST43_SCHEDULE = ("vendredi", "22:31")
+POST44_SCHEDULE = ("samedi", "11:13")
+POST45_SCHEDULE = ("samedi", "11:16")
+POST46_SCHEDULE = ("samedi", "11:18")
+POST47_SCHEDULE = ("samedi", "11:21")
+POST48_SCHEDULE = ("samedi", "11:22")
+POST49_SCHEDULE = ("samedi", "11:27")
+POST50_SCHEDULE = ("samedi", "11:35")
+POST51_SCHEDULE = ("samedi", "11:48")
+POST52_SCHEDULE = ("samedi", "11:57")
+POST53_SCHEDULE = ("dimanche", "16:45")
+POST54_SCHEDULE = ("dimanche", "16:49")
+POST55_SCHEDULE = ("dimanche", "16:54")
+POST56_SCHEDULE = ("dimanche", "17:15")
+# ajoute POST3_SCHEDULE, POST4_SCHEDULE… si besoin
